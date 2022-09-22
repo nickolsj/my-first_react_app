a15aa482445d002f4b3291f042395dbdd2580680
Jack <jacknickols123@gmail.com>
2022-09-22 15:37:07 -0700

    testing jsx button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [counter, setCounter] = useState(-123423);
+
+      return (
+        <div className="App">
+          <button onClick={() => setCounter((prevCount) => (prevCount + 1))}>+</button>
+          <h1>{counter}</h1>
+          <button onClick={() => setCounter((prevCount) => (prevCount - 1))}>-</button>
+        </div>
+      );
 }
 
 export default App;
