1b2a66b3cf2fa5e8a029be7260e9c47603c3ca1e
Jack <jacknickols123@gmail.com>
2022-09-22 19:01:08 -0700

    Restarting app.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import './App.css';
-import { useState } from 'react';
+import React from "react";
 
 const App = () => {
-    const [count, setCounter] = useState(-123423);
-
-      return (
-        <div className="App">
-          <button onClick={() => setCounter((prevCount) => (prevCount + 1))}>+</button>
-          <h1>{counter}</h1>
-          <button onClick={() => setCounter((prevCount) => (prevCount - 1))}>-</button>
-        </div>
-      );
+  return (
+    <h1>App</h1>
+  );
 }
 
 export default App;
